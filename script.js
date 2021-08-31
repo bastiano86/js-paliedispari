963d77e25d10be38d4b50e0675f1a10852b20829
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-31 16:50:54 +0200

    funzione che restituisce un valore random tra 1 e 5

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ console.log("ciao");
 // creo la funzione che chiamerò trovaPalindromo
 function trovaPalindromo(stringa){
     let parolaInvertita = stringa.split("").reverse().join("")
-    return stringa === reversed
+    return stringa === parolaInvertita;
   }
 
-  // chiedo una parola
-  let parolaUno = prompt('scrivi una parola')
+// chiedo una parola
+let parolaUno = prompt('scrivi una parola')
  
-  console.log( trovaPalindromo(parolaUno) );
+console.log( trovaPalindromo(parolaUno) );
+
+  // chiedo un numero da 1 a 5
+  let numeroUtente = prompt('scrivi un numero da 1 a 5');
+
+  console.log(numeroUtente);
+ 
+// creo la funzione che chiamerò trovaPalindromo
+function giocaComputer(a,b) {
+       if (return Math.random() * parseInt((1 - 5) + 1) < 1 ||
+        return Math.random() * parseInt((1 - 5) + 1) > 5) {
+           
+        }
+    }
+
+let numeroComputer = (giocaComputer());
+
+console.log(giocaComputer());
