57786632ec642d2f3cc1a359ebd34fd5a79bc2d2
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-31 16:13:50 +0200

    implementazione del prompt palindromo

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("ciao");
+
+// creo la funzione che chiamerò trovaPalindromo
+function trovaPalindromo(stringa){
+    let parolaInvertita = stringa.split("").reverse().join("")
+    return stringa === reversed
+  }
+
+  // chiedo una parola
+  let parolaUno = prompt('scrivi una parola')
+ 
+  console.log( trovaPalindromo(parolaUno) );
